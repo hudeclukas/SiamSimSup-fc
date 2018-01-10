b6f64f78714fdb87e1b9d4b7ebdfb963a0494685
Lukas Hudec <hudec.lukas21@gmail.com>
2018-01-10 14:22:49 +0100

    validation metrics

--- src/startable.py
+++ src/startable.py
@@ -1,13 +1,16 @@
 import os
 import time
 
-import network as nw
 import tensorflow as tf
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
+import network as nw
 import data_loader as dl
 
-PATH_2_SEG_BIN = "D:/Vision_Images/Berkeley_segmented/BSDS300/segments_squares/"
-MODEL_NAME = "squares_first_run"
+PATH_2_SEG_BIN = "D:/Vision_Images/Berkeley_segmented/BSDS300/segments_c3_smp/"
+MODEL_NAME = "color_smp_15k_x_5e"
 IMAGE_SIZE=(32,32,3)
 
 def main(_arg_):
@@ -42,7 +45,7 @@ def main(_arg_):
     if os.path.exists("model/" + MODEL_NAME + "/checkpoint"):
         saver.restore(sess, model_ckpt)
 
-    if True:
+    if False:
         file_writer = tf.summary.FileWriter('board/logs/' + MODEL_NAME, sess.graph)
         # dl.SUPSIM.visualize=True
         for epoch in range(5):
@@ -76,13 +79,17 @@ def main(_arg_):
     else:
         tp = fp = tn = fn = 0
         siamese.training = False
-
-        for i in range(200):
-            x_s_1, x_s_2, x_l = supsim.next_batch(supsim.test.data, batch_size=128)
+        all_results = None
+        for i in range(5):
+            x_s_1, x_s_2, x_l = supsim.next_batch(supsim.test.data, batch_size=128, image_size=IMAGE_SIZE)
             vec1 = siamese.network1.eval({siamese.x1: x_s_1})
             vec2 = siamese.network2.eval({siamese.x2: x_s_2})
             similarity = sess.run(nw.similarity(vec1, vec2))
             result = list(zip(similarity, x_l))
+            if all_results is None:
+                all_results = result
+            else:
+                all_results = np.concatenate((all_results,result))
             print(result)
             tp += sum(1 for i in result if i[0] < 1 and i[1] == 1)
             fp += sum(1 for i in result if i[0] < 1 and i[1] == 0)
@@ -95,6 +102,31 @@ def main(_arg_):
 
         print('Precision: {:0.6f} Recall: {:0.6f} Accuracy: {:0.6f} F1: {:0.6f}'.format(precision,recall,accuracy,f1_score))
 
+        thr = np.arange(0, 600, 5) / 100
+        tpr,fpr = np.array([tpr_fpr(all_results,th) for th in thr]).transpose()
+
+        auc = metrics.auc(fpr,tpr)
+        plt.figure()
+        plt.plot(fpr,tpr ,color='darkorange',label='ROC squares aoc: {:0.3f}'.format(auc))
+        plt.plot([1,0],[0,1], color='navy', linestyle='--')
+        plt.xlim([0.0,1.0])
+        plt.ylim([0.0,1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right")
+        plt.show()
+        print(thr)
+
+def tpr_fpr(result, threshold):
+    tp = sum(1 for i in result if i[0] < threshold and i[1] == 1)
+    fp = sum(1 for i in result if i[0] < threshold and i[1] == 0)
+    tn = sum(1 for i in result if i[0] >= threshold and i[1] == 0)
+    fn = sum(1 for i in result if i[0] >= threshold and i[1] == 1)
+    tcond = tp + fn
+    tpr = tp / tcond
+    fcond = tn + fp
+    fpr = tn / fcond
+    return tpr, fpr
 
 if __name__ == "__main__":
     tf.app.run()
